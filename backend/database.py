d11c85c62f3a66cff32bb1ffcf67ec31bea9e062
Trinh Tran <trinhtran@bravesoft.com.vn>
2022-12-27 14:55:22 +0700

    compose

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import dotenv_values
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///sales.db'
+config = dotenv_values("../.env")
 # connect_args: chỉ dùng cho sqlite
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(config.get('DATABASE_CONNECTION_URL'), connect_args={"check_same_thread": False})
 # SessionLocal: mở kết nối để thêm - xoá - sửa
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind = engine)
 
