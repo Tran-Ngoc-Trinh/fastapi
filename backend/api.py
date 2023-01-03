f61ff39fe8daa30302204a126ad4dc19e1671073
Trinh Tran <trinhtran@bravesoft.com.vn>
2023-01-03 16:05:12 +0700

    .

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 import models as models, schemas as schemas
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
@@ -11,27 +11,34 @@ from database import engine
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from starlette.middleware.base import BaseHTTPMiddleware
 
 app = FastAPI(title="Trinh Tran")
 
-app.mount("/static", StaticFiles(directory="backend/static"), name="static")
-# <link href="{{ url_for('static', path='/styles.css') }}" rel="stylesheet">
+app.mount("/static", StaticFiles(directory="backend/static", html=True), name="static")
 templates = Jinja2Templates(directory="backend/templates/")
 
 models.Base.metadata.create_all(engine)
 
+class MyMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request):
+        return await super().dispatch(request)
+
 # app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:8080"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
+#     # BaseHTTPMiddleware
+#     # CORSMiddleware,
+#     # allow_origins=["http://localhost:8080", "http://localhost:3000"],
+#     # allow_credentials=True,
+#     # allow_methods=["*"],
+#     # allow_headers=["*"],
 # )
 
 # router default
 @app.get("/",response_class=HTMLResponse)
-def read():
-    return templates.TemplateResponse("index.html", {"request": {}})
+def read(request: Request):
+    id = 3
+    return templates.TemplateResponse("index.html", {"request": request,"id": id})
+    # return "abcd"
 
 
 # router riêng
